df06639674104172efc54c2fc305344fdf9cf63b
Nevermind <58900773+RalivTV@users.noreply.github.com>
2021-10-27 18:44:56 +0100

    Add files via upload

---
+++ balas.sql
@@ -0,0 +1,12 @@
+USE `redm_extended`;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('22_ammo', 'Balas 22', 18, 0, 1),
+	('arrows', 'Flechas', 8, 0, 1),
+	('knives', 'Facas', 15, 0, 1),
+	('pistol_ammo', 'Balas Pistola', 10, 0, 1),
+	('repeator_ammo', 'Balas Repeator', 20, 0, 1),
+	('revolver_ammo', 'Balas Revolver', 10, 0, 1),
+	('rifle_ammo', 'Balas Rifle', 20, 0, 1),
+	('shotgun_ammo', 'Balas Caçadeira', 22, 0, 1),
+	('snipe_ammo', 'Balas Sniper', 30, 0, 1);
